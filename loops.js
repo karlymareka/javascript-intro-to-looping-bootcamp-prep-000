ccd0cc05557b9218260d2393f76930cc9f043a74
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-28 00:28:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 25 ; i++) {
-   console.log(`"I am ${i} strange loop"`);
+   if (i === 1) {
+     console.log("I am 1 strange loop.");
+   }
+   else {
+     console.log(`"I am ${i} strange loops."`);
   } 
 }
